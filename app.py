78ded95994989e1ef947de1e1a04904f69a357fe
Анна Вотчинникова <annavotcinnikova5@gmail.com>
2025-10-09 21:20:20 +0700

    Имя студента

--- app.py
+++ app.py
@@ -309,4 +309,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Анна Вотчинникова'
+    return render_template('example.html', name=name)
