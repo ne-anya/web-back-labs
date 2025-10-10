f8a8f257325ea936c9d926d114539d524d3c214b
Анна Вотчинникова <annavotcinnikova5@gmail.com>
2025-10-10 20:46:25 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -323,3 +323,7 @@ def example():
     return render_template('example.html', 
                            name=name, lab=lab, group=group, 
                            course=course, fruits=fruits)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
