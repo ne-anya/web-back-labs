0fd7bea3581f60fbaa6a22e3f763185004764108
Анна Вотчинникова <annavotcinnikova5@gmail.com>
2025-12-24 23:15:59 +0700

    Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 import datetime
 import os
 
@@ -20,6 +21,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 
 @app.route("/")
@@ -44,6 +46,7 @@ def index():
                 <ol><a href="/lab4/">Четвертая лаборатораная</a></ol>
                 <ol><a href="/lab5/">Пятая лабораторная</a></ol>
                 <ol><a href="/lab6/">Шестая лабораторная</a></ol>
+                <ol><a href="/lab7/">Седьмая лабораторная</a></ol>
             </li>
         </main>
         <footer>
