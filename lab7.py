6aa4139612243bb017339f8434e8bf310468bca6
Анна Вотчинникова <annavotcinnikova5@gmail.com>
2025-12-26 00:36:44 +0700

    Добавление нового фильма по REST

--- lab7.py
+++ lab7.py
@@ -111,3 +111,11 @@ def put_film(id):
     film = request.get_json()
     films[id] = film
     return films[id]
+
+
+@lab7.route('/lab7/rest-api/films/', methods=['POST'])
+def add_film():
+    new_film = request.get_json()
+    films.append(new_film)
+    new_id = len(films) - 1
+    return {"id": new_id, "message": "Фильм добавлен", "total_films": len(films)}, 201
