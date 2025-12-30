a6f2c988e7eec2415421b5b5a2224e63a3cd5091
Анна Вотчинникова <annavotcinnikova5@gmail.com>
2025-12-30 22:01:06 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -65,3 +65,10 @@ def login():
 @login_required  # рис. 41, стр. 19
 def article_list():
     return "список статей"
+
+
+@lab8.route('/lab8/logout/')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab8/')
