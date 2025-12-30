8a1c4f564a0edbfae356221d20f8c0956ecec77a
Анна Вотчинникова <annavotcinnikova5@gmail.com>
2025-12-31 00:49:13 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from lab4 import lab4
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 import datetime
 import os
@@ -56,6 +57,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 
 @app.route("/")
